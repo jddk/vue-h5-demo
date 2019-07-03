2bf7f2d31a3e9bd7e1d1556c5aa928de6c47d0ca
陈世亮 <352056038@qq.com>
2019-07-03 15:16:57 +0800

    vue+vant

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import './vant.js'
+import './rem.js'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<style lang="scss" scoped>
+  .box{
+    width: 100px;
+    height: 100px;
+    background-color: pink;
+  }
+</style>
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <div class="box"></div>
+    <van-button type="default">默认按钮</van-button>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
